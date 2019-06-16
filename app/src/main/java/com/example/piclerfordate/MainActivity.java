e067e7f848f95d79ddf84e52f5c81b1e1816bb7e
Zi Wan <yipzw-wm17@student.tarc.edu.my>
2019-06-16 23:29:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/piclerfordate/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.piclerfordate;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.datepicker));
+    }
+}
